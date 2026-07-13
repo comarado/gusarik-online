1c8dad317ff3ee5016892520eab53b3a2937f632
comarado <comarado@gmail.com>
2026-07-13 19:44:53 -0300

    Update build.gradle.kts

--- engine/game/build.gradle.kts
+++ engine/game/build.gradle.kts
@@ -6,8 +6,11 @@ dependencies {
     implementation(project(":engine:scoring"))
     implementation(project(":engine:validation"))
     implementation(project(":core:domain"))
+    implementation(project(":engine:ai"))
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation(libs.junit)
     testImplementation(libs.coroutines.test)
     testImplementation(libs.mockk)
+
 }
