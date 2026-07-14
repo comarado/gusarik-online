18d8b62d4602c34619825770fc574608b7f1cdcf
comarado <comarado@gmail.com>
2026-07-13 22:48:05 -0300

    Update GameViewModel.kt

--- feature/game/src/main/kotlin/com/gusarik/feature/game/ui/GameViewModel.kt
+++ feature/game/src/main/kotlin/com/gusarik/feature/game/ui/GameViewModel.kt
@@ -14,6 +14,7 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
+import com.gusarik.engine.scoring.model.ScoreType.BULLET
 
 data class GameUiState(
     val gameState: GameState? = null,
